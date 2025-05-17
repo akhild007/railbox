5f7a5e0e38005cd4a1a07a5f23761d7f6398aee5
Akhil Dasari <akhildasari@MacBookAir.lan>
2025-05-17 17:17:06 +0530

    add logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,12 +37,18 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  # config.logger = ActiveSupport::Logger.new(STDOUT)
+  #   .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+  #   .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+
+  config.logger = ActiveSupport::Logger.new("log/production.log")
+  config.logger.formatter = ::Logger::Formatter.new
+  config.log_tags = [:request_id]
+  config.logger = ActiveSupport::TaggedLogging.new(config.logger)
+
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  # config.log_tags = [ :request_id ]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
