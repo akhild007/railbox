bff0ce7419fe9725a1350d984d4da6b8d405da3c
Akhil Dasari <akhildasari@MacBookAir.lan>
2025-05-17 15:40:54 +0530

    whitelist root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[home]
 
   def home
     render json: { status: true, message: "Hello World", timestamp: Time.zone.now.to_i }, status: 200
